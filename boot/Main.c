49e4a7adcc944bfb4cfbfb278a2f984acb15aa02
MINJAEKIM <innersme@gmail.com>
2020-04-04 18:21:54 +0900

    debug_printf 구현

--- boot/Main.c
+++ boot/Main.c
@@ -17,6 +17,8 @@ void main(void)
     Hal_uart_put_char('\n');
     putstr("Hello world\n");
 
+    Printf_test();
+
     i = 100;
     while(i--)
     {
@@ -28,4 +30,18 @@ void main(void)
 static void Hw_init()
 {
     Hal_uart_init();
+}
+
+static void Printf_test(void)
+{
+    char* str = "printf pointer test";
+    char* nullptr = 0;
+    uint32_t i = 5;
+
+    debug_printf("%s\n", "Hello printf");
+    debug_printf("output string pointer: %s\n", str);
+    debug_printf("%s is null pointer, %u number\n", nullptr, 10);
+    debug_printf("%u = 5\n", i);
+    debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
+    debug_printf("print zero %u\n", 0);
 }
