ec61157d20ec96d61eaf02c698cccfc331faaff4
minjae <innersme@gmail.com>
2020-10-20 17:06:30 +0900

    chap5

--- lib/stdio.c
+++ lib/stdio.c
@@ -1,8 +1,14 @@
+/*
+ * stdio.c
+ *
+ *  Created on: Sep 17, 2018
+ *      Author: maanu
+ */
+
 #include "stdint.h"
 #include "HalUart.h"
 #include "stdio.h"
 
-#define NULL            0
 #define PRINTF_BUF_LEN  1024
 
 static char printf_buf[PRINTF_BUF_LEN];   // 1KB
@@ -32,23 +38,22 @@ uint32_t vsprintf(char* buf, const char* format, va_list arg)
 {
     uint32_t c = 0;
 
-    char        ch;
-    char*       str;
-    uint32_t    uint;
-    uint32_t    hex;
+    char     ch;
+    char*    str;
+    uint32_t uint;
+    uint32_t hex;
 
-    for (uint32_t i = 0; format[i]; i++)
+    for (uint32_t i = 0 ; format[i] ; i++)
     {
         if (format[i] == '%')
         {
             i++;
-            switch (format[i])
+            switch(format[i])
             {
             case 'c':
                 ch = (char)va_arg(arg, int32_t);
                 buf[c++] = ch;
                 break;
-            
             case 's':
                 str = (char*)va_arg(arg, char*);
                 if (str == NULL)
@@ -60,12 +65,10 @@ uint32_t vsprintf(char* buf, const char* format, va_list arg)
                     buf[c++] = (*str++);
                 }
                 break;
-
             case 'u':
                 uint = (uint32_t)va_arg(arg, uint32_t);
                 c += utoa(&buf[c], uint, utoa_dec);
                 break;
-
             case 'x':
                 hex = (uint32_t)va_arg(arg, uint32_t);
                 c += utoa(&buf[c], hex, utoa_hex);
@@ -90,29 +93,30 @@ uint32_t vsprintf(char* buf, const char* format, va_list arg)
 
 uint32_t utoa(char* buf, uint32_t val, utoa_t base)
 {
-    uint32_t c = 0;
-    int32_t idx= 0;
-    char    tmp[11];    // It is big enough for store 32 bit int
+    const char asciibase = 'a';
 
-    do
-    {
+    uint32_t c = 0;
+    int32_t idx = 0;
+    char     tmp[11];   // It is enough for 32 bit int
+
+    do {
         uint32_t t = val % (uint32_t)base;
         if (t >= 10)
         {
-            t += 'A' - '0' - 10;
+            t += asciibase - '0' - 10;
         }
         tmp[idx] = (t + '0');
         val /= base;
         idx++;
-    } while (val);
-    
-    // reverse;
+    } while(val);
+
+    // reverse
     idx--;
-    while(idx >= 0)
+    while (idx >= 0)
     {
         buf[c++] = tmp[idx];
         idx--;
     }
-    
+
     return c;
 }
--- lib/stdio.h
+++ lib/stdio.h
@@ -1,3 +1,10 @@
+/*
+ * stdio.h
+ *
+ *  Created on: Sep 17, 2018
+ *      Author: maanu
+ */
+
 #ifndef LIB_STDIO_H_
 #define LIB_STDIO_H_
 
